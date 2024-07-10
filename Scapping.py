65135220be8585587035acf73783eccfb3c5514f
Wenyu Chiou <wenyuchiou12@gmail.com>
2024-07-10 14:59:46 +0800

    saving scraping data according to names

--- Scapping.py
+++ Scapping.py
@@ -1,6 +1,21 @@
 # Scraping historical data for certain stocks
 
 import yfinance as yf
+import pandas as pd
+import os
 
-df = yf.Ticker("^SPX").history(period="max")
-print(df)
+# Create the dict of stock name you want to scrap
+stock_data = dict()
+stock_data['name'] = ["2330.TW",'^SPX']
+
+dir = r"C:\Users\user\Desktop\investment\python\scrapping\scraping data"
+# Scraping the stock data using yfinance modulus given start and end data
+for number, name in enumerate(stock_data['name']):
+    df = yf.Ticker(name).history(start="2020-01-01",end="2024-01-01")
+    name = name.replace('^','')
+    df.to_csv(os.path.join(dir,name + '.csv')) # save the scraping data as a csv file
+
+
+
+
+
